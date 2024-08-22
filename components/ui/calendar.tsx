fa8d47b3d16a23320ee1a8b60b964e67fe996b4f
Kaustav Ghosh <teetangh@gmail.com>
2024-08-22 05:52:20 +0530

    refactor: Update react-day-picker version to 8.10.1 and remove unused code

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,21 +1,13 @@
-"use client";
+"use client"
 
-import * as React from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import { DayPicker } from "react-day-picker";
+import * as React from "react"
+import { ChevronLeft, ChevronRight } from "lucide-react"
+import { DayPicker } from "react-day-picker"
 
-import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
 
-export type CalendarProps = React.ComponentProps<typeof DayPicker>;
-
-function IconLeft({ ...props }) {
-  return <ChevronLeft className="h-4 w-4" />;
-}
-
-function IconRight({ ...props }) {
-  return <ChevronRight className="h-4 w-4" />;
-}
+export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
 function Calendar({
   className,
@@ -62,13 +54,13 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft,
-        IconRight,
+        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
-  );
+  )
 }
-Calendar.displayName = "Calendar";
+Calendar.displayName = "Calendar"
 
-export { Calendar };
+export { Calendar }
