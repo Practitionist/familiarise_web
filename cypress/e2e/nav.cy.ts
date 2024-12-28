a7d65cb7c0e9ef8a2120fdc7b4f6cf3def1ff41d
teetangh <teetangh@gmail.com>
2024-12-28 20:59:22 +0530

    added cypress

---
+++ cypress/e2e/nav.cy.ts
@@ -0,0 +1,7 @@
+import "@testing-library/cypress";
+
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("https://example.cypress.io");
+  });
+});
--- tests/typescript/sum.test.ts
+++ tests/typescript/sum.test.ts
@@ -1,7 +1,6 @@
 // Generate sum test code
-import { sum } from './sum';
-import { expect, test } from '@jest/globals';
+import { sum } from "./sum";
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).equal(3);
 });
