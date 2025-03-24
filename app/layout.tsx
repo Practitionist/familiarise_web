a51298f87928cad71eb5663c8322ce401fc3bc24
Kaustav Ghosh <teetangh@gmail.com>
2025-03-24 14:06:24 +0530

    Remove unused video conferencing styles from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ import { getServerSession } from "next-auth";
 import { Inter } from "next/font/google";
 import authOptions from "./api/auth/[...nextauth]/options";
 
-import "@stream-io/video-react-sdk/dist/css/styles.css";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
